9e1155e29f9e7c7dd323d7d971dfbdf53f2236d1
Abigail <abialvarado14@unitec.edu>
2017-04-28 08:37:52 -0500

    Update

---
+++ Lab2-SuyapaAlvarado.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+	int main(){
+		int opc, x;
+		cout << "Ingrese el ejercicio que desea: \n1.Numerdo de Euler\n2.Area de trapezoide: ";
+		cin >> opc;
+
+		if (opc==1){
+			cout << "Ingrese el valor de x: ";
+			cin >> x;
+		}
+	
+
+	}
